6904f0d3d9a6eaa407543e65d9b7d6b353cb72bd
Kenadia <Kenadia@users.noreply.github.com>
2026-02-04 10:58:09 -0800

    Fix for non-interactive shells

--- bashrc-main.sh
+++ bashrc-main.sh
@@ -1,3 +1,6 @@
+# Exit immediately if not an interactive shell.
+[[ $- != *i* ]] && return
+
 # Run environment-specific setup.
 for f in ~/.myconfig/bashrc-shared.sh ~/.myconfig/bashrc-laptop.sh ~/.myconfig/bashrc-cloud.sh
 do
